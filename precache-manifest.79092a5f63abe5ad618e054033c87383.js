601b2a1fa17c9a341cdfb39a6256b17894db1323
Awu Chen <awwu@Mac.lan>
2026-09-17 21:12:16 -0400

    Updates

---
+++ precache-manifest.79092a5f63abe5ad618e054033c87383.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03730f88d3e6be2c71e7acd6cb172e1",
+    "url": "/DemoNet/index.html"
+  },
+  {
+    "revision": "0c966cdba70d44f0f615",
+    "url": "/DemoNet/static/css/main.ead59979.chunk.css"
+  },
+  {
+    "revision": "a21349a35aa772a3a04a",
+    "url": "/DemoNet/static/js/2.3d8e33b0.chunk.js"
+  },
+  {
+    "revision": "7b36c4a4b00d4c1eb1f21a15ccb34795",
+    "url": "/DemoNet/static/js/2.3d8e33b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c966cdba70d44f0f615",
+    "url": "/DemoNet/static/js/main.fc0014cb.chunk.js"
+  },
+  {
+    "revision": "a097ff89d31a9a152507",
+    "url": "/DemoNet/static/js/runtime-main.fff16487.js"
+  }
+]);
